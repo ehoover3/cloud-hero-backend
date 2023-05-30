47aaa6d3f6971b6edd856e3d79d865222d1d1141
Eric Hoover <ehoover3@yahoo.com>
2023-05-30 19:03:37 -0400

    test

--- app.js
+++ app.js
@@ -1,148 +1,99 @@
 const express = require("express");
-const app = express();
-
 const AWS = require("aws-sdk");
-
-require("dotenv").config();
-
-const { v4: uuidv4 } = require("uuid");
-const bodyParser = require("body-parser");
-
+const dotenv = require("dotenv");
 const port = process.env.PORT || 3000;
 
-// Configure AWS SDK
+dotenv.config();
+
+const app = express();
+const tableName = "cloudhero";
+app.use(express.json());
+
 if ((port = 3000)) {
   AWS.config.update({
     region: process.env.AWS_REGION,
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   });
+} else {
+  AWS.config.update({ region: "us-east-1" }); // Update with your desired region
 }
 
-// Create an instance of the DynamoDB DocumentClient
-const docClient = new AWS.DynamoDB.DocumentClient();
-const tableName = process.env.DYNAMODB_TABLE_NAME;
+const dynamodb = new AWS.DynamoDB.DocumentClient();
 
-app.use(bodyParser.json());
-
-app.post("/", (req, res) => {
-  const itemId = uuidv4();
-  const params = {
-    TableName: tableName,
-    Item: {
-      id: itemId,
-      message: req.body.message,
-    },
-  };
-
-  docClient.put(params, (err, data) => {
-    if (err) {
-      console.error("Error creating item:", err);
-      res.status(500).send("Error creating item");
-    } else {
-      console.log("Item created successfully");
-      res.status(200).send("Item created successfully");
-    }
-  });
-});
-
-// app.get('/', (req, res)=>res.send('test 123'))
-
-app.get("/", (req, res) => {
+app.get("/items", (req, res) => {
   const params = {
     TableName: tableName,
   };
 
-  docClient.scan(params, (err, data) => {
+  dynamodb.scan(params, (err, data) => {
     if (err) {
-      console.error("Error retrieving items:", err);
-      res.status(500).send("Error retrieving items");
+      console.error(err);
+      res.status(500).json({ error: "Unable to retrieve items" });
     } else {
-      console.log("Items retrieved successfully:", data.Items);
-      res.status(200).send(data.Items);
+      res.json(data.Items);
     }
   });
 });
 
-app.get("/:id", (req, res) => {
+app.post("/items", (req, res) => {
+  const { id, name } = req.body;
+
   const params = {
     TableName: tableName,
-    Key: {
-      id: req.params.id,
-    },
+    Item: { id, name },
   };
 
-  docClient.get(params, (err, data) => {
+  dynamodb.put(params, (err) => {
     if (err) {
-      console.error("Error retrieving item:", err);
-      const errorMessage = `Error retrieving item: ${err.message}`;
-      const errorDetails = {
-        name: err.name,
-        message: err.message,
-        stack: err.stack,
-        code: err.code,
-        statusCode: err.statusCode,
-        retryable: err.retryable,
-        retryDelay: err.retryDelay,
-        time: err.time,
-        // Add any other custom properties from the err object if available
-      };
-      res.status(500).json({ error: errorMessage, details: errorDetails });
+      console.error(err);
+      res.status(500).json({ error: "Unable to create item" });
     } else {
-      if (data.Item) {
-        console.log("Item retrieved successfully:", data.Item);
-        res.status(200).send(data.Item);
-      } else {
-        console.log("Item not found");
-        res.status(404).send("Item not found");
-      }
+      res.sendStatus(201);
     }
   });
 });
 
-app.put("/:id", (req, res) => {
+app.put("/items/:id", (req, res, next) => {
+  const { id } = req.params;
+  const { name } = req.body;
+
   const params = {
     TableName: tableName,
-    Key: {
-      id: req.params.id,
-    },
-    UpdateExpression: "set message = :value1",
-    ExpressionAttributeValues: {
-      ":value1": req.body.message,
-    },
+    Key: { id },
+    UpdateExpression: "set #name = :nameValue",
+    ExpressionAttributeNames: { "#name": "name" },
+    ExpressionAttributeValues: { ":nameValue": name },
   };
 
-  docClient.update(params, (err, data) => {
+  dynamodb.update(params, (err) => {
     if (err) {
-      console.error("Error updating item:", err);
-      res.status(500).send("Error updating item");
-    } else {
-      console.log("Item updated successfully");
-      res.status(200).send("Item updated successfully");
+      console.error(err);
+      return next(err);
     }
+
+    res.sendStatus(200);
   });
 });
 
-app.delete("/:id", (req, res) => {
+app.delete("/items/:id", (req, res, next) => {
+  const { id } = req.params;
+
   const params = {
-    TableName: tableName,
-    Key: {
-      id: req.params.id,
-    },
+    TableName: "cloudhero",
+    Key: { id },
   };
 
-  docClient.delete(params, (err, data) => {
+  dynamodb.delete(params, (err) => {
     if (err) {
-      console.error("Error deleting item:", err);
-      res.status(500).send("Error deleting item");
-    } else {
-      console.log("Item deleted successfully");
-      res.status(200).send("Item deleted successfully");
+      console.error(err);
+      return next(err);
     }
+
+    res.sendStatus(200);
   });
 });
 
-// Start the server
 app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
